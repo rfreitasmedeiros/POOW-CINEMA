bb78446b56eff1d9b744b9e7812415330e58d36e
Saul Goodman <trabalhos.trigo@gmail.com>
2025-04-02 21:17:48 -0500

    .

--- index.php
+++ index.php
@@ -5,11 +5,11 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>CineMark</title>
+    <title>CineTech Solutions</title>
 </head>
 
 <body>
-    <h1>CineMark</h1>
+    <h1>CineTech Solutions</h1>
     <nav>
         <ul>
             <li><a href="index.html">Início</a></li>
@@ -18,7 +18,7 @@
             <li><a href="sobre.html">Sobre</a></li>
         </ul>
     </nav>
-    <h2>Bem-vindo ao CineMark</h2>
-    <p>O CineMark é um sistema de gerenciamento de cinema que permite aos usuários <br> 
+    <h2>Bem-vindo ao CineTech Solutions</h2>
+    <p>O CineTech Solutions é um  site de gerenciamento de cinema que permite aos usuários <br> 
         cadastrarem e visualizarem informações sobre filmes, tais quais como sinopse, elenco e etc</p>
 </body>
